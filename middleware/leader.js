a768e75c8e635f1d53ed9a3029d6fa912b1678e2
spacer-ranking <maiiduda2008@gmail.com>
2026-03-02 12:00:03 +0300

    Delete middleware directory
    Signed-off-by: spacer-ranking <maiiduda2008@gmail.com>

--- middleware/leader.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  if (!req.session.user || req.session.user.role !== 'leader') {
-    return res.status(403).send('Доступ запрещен. Только для лидера.');
-  }
-  next();
-};
